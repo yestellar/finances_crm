7021481336d226f92364c26f6b149f736a527685
Dmytro Smirnov <vonrimsd17@gmail.com>
2020-07-24 13:45:39 +0200

    Paginations as mixins in History view realized

--- src/views/History.vue
+++ src/views/History.vue
@@ -13,32 +13,45 @@
     <p class="center" v-else-if="!records.length">No records yet</p>
 
     <section v-else>
-      <HistoryTable :records="records" />
+      <HistoryTable :records="items" />
+
+      <Paginate
+        v-model="page"
+        :page-count="pageCount"
+        :click-handler="pageChangeHandler"
+        :prev-text="'Prev'"
+        :next-text="'Next'"
+        :container-class="'pagination'"
+        :page-class="'waves-effect'"
+      />
     </section>
   </div>
 </template>
 
 <script>
+import paginationMixin from '@/mixins/pagination.mixin'
 import HistoryTable from '@/components/HistoryTable'
 
 export default {
   name: 'history',
+  mixins: [paginationMixin],
   data: () => ({
     loading: true,
     records: [],
     categories: []
   }),
   async mounted() {
+    this.records = await this.$store.dispatch('fetchRecords')
     this.categories = await this.$store.dispatch('fetchCategories')
-    const records = await this.$store.dispatch('fetchRecords')
-    this.records = records.map(record => {
+
+    this.setupPagination(this.records.map(record => {
       return {
         ...record,
         categoryName: this.categories.find(c => c.id === record.categoryId).title,
         typeClass: record.type === 'income' ? 'green' : 'red',
         typeText: record.type === 'income' ? 'Income' : 'Outcome',
       }
-    })
+    }))
 
     this.loading = false
   },
